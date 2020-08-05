ee488019cbcad9a12382ab25aa01f94d639fdb04
mohanpierce99 <paying.rich6990@gmail.com>
2020-08-05 20:42:05 +0530

    Start stop UI component

--- sonicdesign/index.js
+++ sonicdesign/index.js
@@ -4,14 +4,19 @@ const worker = new Worker("../worker.js");
 
 var listenJson;
 var gooJson;
-async function loadLottie () {
+async function loadLottie() {
     let listen = await fetch("./listen.json")
     listenJson = await listen.json();
     let goo = await fetch("./googleassist.json")
     gooJson = await goo.json();
+    console.log("hej monika")
 }
 
+
+
+
 function bootstrap(p) {
+
     let config = {
         alphabet: '^ABC123$',
         data: 'A',
@@ -34,22 +39,30 @@ function bootstrap(p) {
         }).join(";");
         sheet.insertRule(selector + "{" + propText + "}", sheet.cssRules.length);
     };
+
+
     $(document).ready(function () {
         $('#pagepiling').pagepiling({
             scrollingSpeed: 300,
             navigation: false,
             keyboardScrolling: false,
         });
+        console.log("heydcnmdbcn")
+
+
+
+
+
         $('.bolt').each(function (e) {
-    
+
             var bolt = $(this),
                 div = $(this).children('div');
-    
+
             bolt.addClass('animate');
-    
+
             var tween = new TimelineMax({
                 onComplete() {
-                    bolt.removeClass('animate');         
+                    bolt.removeClass('animate');
                 }
             }).set(div, {
                 rotation: 360
@@ -67,16 +80,16 @@ function bootstrap(p) {
                 rotation: 0,
                 y: 0
             });
-    
-            
-                
+
+
+
             // bolt.addClass('animate');      // Energy changes
             // tween.restart();                //TWEEN
-                
+
         })
 
     })
-    
+
 
 
 
@@ -88,10 +101,10 @@ function bootstrap(p) {
         console.log("RECORDED", params, energy, success);
 
         //flash
-        bolt.addClass('animate');      
-        tween.restart();                
+        bolt.addClass('animate');
+        tween.restart();
         // Toggle screen
-        if(energy > config.energyFilter){      
+        if (energy > config.energyFilter) {
             addRule(".containblack::before", {
                 opacity: "1"
             });
@@ -102,7 +115,7 @@ function bootstrap(p) {
                 opacity: "0"
             });
             document.querySelector("#energyid").innerHTML = energy;
-            document.querySelector("#energyalert").innerHTML = `Energy less than ${energy}. Excellent! Keep up the distance`    
+            document.querySelector("#energyalert").innerHTML = `Energy less than ${energy}. Excellent! Keep up the distance`
         }
     }
 
@@ -113,7 +126,7 @@ function bootstrap(p) {
             ls.style.visibility = "visible"
             // intext.textContent = " Chirping.. "
             // ls.load(gooJson);
-        } else{
+        } else {
             ls.style.visibility = "hidden"
         }
     }
@@ -121,23 +134,21 @@ function bootstrap(p) {
     p.setup = () => {
         analyzer.setup();
     }
-    document.querySelector("#preactivate").onclick = function() {
-        analyzer.init(cb, switchF).then(() => {
-            console.log("setupDone")
-        })
-    }
+
+
 
     // p.touchStarted = () => {
     //     p.getAudioContext().resume();
     // }
 
-   
+
 
     worker.addEventListener("message", (e) => {
         analyzer.draw();
     })
 
-    document.querySelector(".circle").onclick = () => {
+    document.querySelector(".circle").onclick = async () => {
+        await analyzer.init(cb, switchF)
         analyzer.start()
         // worker.postMessage("start");
 
@@ -160,7 +171,46 @@ function bootstrap(p) {
                     setTimeout(() => {
                         $.fn.pagepiling.moveSectionDown();
                         setTimeout(() => {
-                            document.querySelector("#activate").innerHTML = "<lottie-player src='https://assets7.lottiefiles.com/packages/lf20_ydTi0b.json'  background='transparent'  speed='1'  style='width: 300px; height: 300px;'  loop  autoplay></lottie-player>";
+                            document.querySelector("#activate").innerHTML = "<div class='tooltip'><div class='wrapperIcon play-id'><i class='fas fa-play animateClick controllIcons'></i></div><div class='wrapperIcon stop-id'><i class='fas fa-stop controllIcons'></i></div></div><lottie-player src='https://assets7.lottiefiles.com/packages/lf20_ydTi0b.json' id='bear'  background='transparent'  speed='1'  style='width: 300px; height: 300px;'  loop  autoplay></lottie-player>";
+                            var toggleState = true;
+                            document.querySelector(".tooltip").addEventListener("click", function (e) {
+                                console.warn(e.target)
+                                if (e.target.classList.contains("play-id") || e.target.classList.contains("fa-play")) {
+                                    document.querySelector("#acttext").innerHTML = "Busy, listening and sending sonic energy waves"
+                                    if (toggleState == true) {
+                                        return;
+                                    }
+                                    if (document.querySelector(".fa-stop").classList.contains("animateClick")) {
+                                        document.querySelector(".fa-stop").classList.remove("animateClick");
+                                    }
+                                    document.querySelector(".fa-play").classList.add("animateClick");
+                                    bearAnimate(true);
+                                    // Do anything for play
+                                    toggleState = true;
+                                } else if (e.target.classList.contains("stop-id") || e.target.classList.contains("fa-stop")) {
+                                    document.querySelector("#acttext").innerHTML = "Sleeping......"
+                                    if (toggleState == false) {
+                                        return;
+                                    }
+                                    if (document.querySelector(".fa-play").classList.contains("animateClick")) {
+                                        document.querySelector(".fa-play").classList.remove("animateClick");
+                                    }
+                                    document.querySelector(".fa-stop").classList.add("animateClick");
+                                    bearAnimate(false);
+                                    //Do anything to stop
+                                    toggleState = false;
+                                }
+                            });
+
+                            function bearAnimate(start) {
+                                if (start) {
+                                    document.querySelector("#bear").play();
+                                    document.querySelector("#bear").setLooping(true);
+                                } else {
+                                    document.querySelector("#bear").setLooping(false);
+                                }
+                            }
+
                             document.querySelector("#acttext").innerHTML = "Busy, listening and sending sonic energy waves"
                             document.querySelectorAll(".fonts-social")
                                 .forEach((el) => {
@@ -169,7 +219,7 @@ function bootstrap(p) {
                                             "darkcolor"
                                         )
                                 });
-                            
+
                         }, 700);
                     }, 2800)
                 }, 200);
