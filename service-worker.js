9c90cc0af64de4d122ffd31c63bd3aca954ae853
ak5123 <kanduarul@gmail.com>
2020-08-13 02:38:33 +0530

    flash screen update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cache-v3";
+const CACHE_NAME = "cache-v4";
 const CACHE_FILES = [
     '/',
     '/index.html',
@@ -29,6 +29,32 @@ const CACHE_FILES = [
     '/vendor/jquery.pagepiling.min.css',
     '/dist/sonicsensor.min.js',
     '/manifest.webmanifest',
+    '/assets/apple-icon-180.jpg',
+    '/assets/apple-icon-167.jpg',
+    '/assets/apple-icon-152.jpg',
+    '/assets/apple-icon-120.jpg',
+    '/assets/apple-splash-2048-2732.jpg',
+    '/assets/apple-splash-2732-2048.jpg',
+    '/assets/apple-splash-1668-2388.jpg',
+    '/assets/apple-splash-2388-1668.jpg',
+    '/assets/apple-splash-1536-2048.jpg',
+    '/assets/apple-splash-2048-1536.jpg',
+    '/assets/apple-splash-1668-2224.jpg',
+    '/assets/apple-splash-2224-1668.jpg',
+    '/assets/apple-splash-1620-2160.jpg',
+    '/assets/apple-splash-2160-1620.jpg',
+    '/assets/apple-splash-1242-2688.jpg',
+    '/assets/apple-splash-2688-1242.jpg',
+    '/assets/apple-splash-1125-2436.jpg',
+    '/assets/apple-splash-2436-1125.jpg',
+    '/assets/apple-splash-828-1792.jpg',
+    '/assets/apple-splash-1792-828.jpg',
+    '/assets/apple-splash-1080-1920.jpg',
+    '/assets/apple-splash-1920-1080.jpg',
+    '/assets/apple-splash-750-1334.jpg',
+    '/assets/apple-splash-1334-750.jpg',
+    '/assets/apple-splash-640-1136.jpg',
+    '/assets/apple-splash-1136-640.jpg'
 ];
 
 self.addEventListener('install', function (event) {
